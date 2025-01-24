4fd2bd4ed2c05e538204c776bff382ee988eab94
Christine Lin <lin.christine19@gmail.com>
2025-01-24 16:47:32 -0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,104 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [latestNews, setLatestNews] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const categories = [
+    "regional",
+    "technology",
+    "lifestyle",
+    "business",
+    "general",
+    "programming",
+    "science",
+    "entertainment",
+    "world",
+    "sports",
+    "finance",
+    "academia",
+    "politics",
+    "health",
+    "opinion",
+    "food",
+    "game",
+  ];
+
+  const countries = {
+    Australia: "AS",
+    Canada: "CA",
+    China: "CN",
+    "South Korea": "SK",
+    Taiwan: "TW",
+    "United State": "US",
+  };
+
+  async function fetchLatestNews(category, country) {
+    try {
+      const response = await fetch(`/api/latest-news?category=${category}&country=${country}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch latest news");
+      }
+      const data = await response.json();
+      setLatestNews(data);
+    } catch (err) {
+      console.error("Error fetching latest news:", err);
+    }
+  }
+
+  useEffect(() => {
+    fetchLatestNews(selectedCategory, selectedCountry);
+  }, [selectedCategory, selectedCountry]);
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  }
+
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  }
+
+  return (
+    <>
+      <div>
+        <h1>The Latest News</h1>
+        <form>
+          <label htmlFor="category">Select Category</label>
+          <select name="category" id="category" onChange={handleCategoryChange}>
+            <option value="">All</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="country">Select Country</label>
+          <select name="countries" id="countries" onChange={handleCountryChange}>
+            <option value="">All</option>
+            {Object.entries(countries).map(([country, code]) => (
+              <option key={code} value={code}>
+                {country}
+              </option>
+            ))}
+          </select>
+        </form>
+
+        {latestNews.map((news) => (
+          <div key={news.id}>
+            <h2>{news.title}</h2>
+            <img className="news-image" src={news.image} alt="news-img" />
+            <p>{news.description}</p>
+            <p>{news.category.join(", ")}</p>
+            <p>{news.published}</p>
+            <a href={news.url}>View the Full Story</a>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.currentsapi.services/v1";
+
+app.use(express.json());
+
+const getLatestNews = async (category, country) => {
+  const apiKey = process.env.API_KEY;
+  let url = `${API_URL}/latest-news?apiKey=${apiKey}`;
+
+  if (category) url += `&category=${category}`;
+  console.log(`The category is: ${category}`);
+
+  if (country) url += `&country=${country}`;
+  console.log(`The country is: ${country}`);
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch latest news");
+  }
+  const data = await response.json();
+  return data.news;
+};
+
+app.get("/api/latest-news", async (req, res) => {
+    const {category, country} = req.query;
+  try {
+    const news = await getLatestNews(category, country);
+    res.json(news);
+  } catch (err) {
+    console.error("Error fetching latest news:", err);
+    res.status(500).json({ error: "Failed to fetch latest news" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
